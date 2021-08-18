348bc55b5fb6d894dbe6c416d2f22b6660a9db7d
otx-koll <ihs1836@gmail.com>
2021-08-18 23:53:51 +0900

    update

---
+++ code/chap04/src/main/java/spring/RegisterRequest.java
@@ -0,0 +1,40 @@
+package spring;
+
+public class RegisterRequest {
+	// 회원 가입 처리를 할 때, 필요한 이메일, 암호, 이름 데이터를 담고 있는 클래스
+	
+	private String email;
+	private String password;
+	private String confirmPassword;
+	private String name;
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public boolean isPasswordEqualToConfirmPassword() {
+		return password.equals(confirmPassword);
+	}
+	
+}
